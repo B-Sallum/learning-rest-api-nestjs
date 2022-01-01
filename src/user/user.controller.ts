4b25fc42b22a290e4ec7b2b9f731a023e521abcd
B-Sallum <brusallum@hotmail.com>
2022-01-01 20:05:02 -0300

    Learning hash with bcrypt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { UserService } from './user.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
   constructor(private service: UserService) {}
 
   @Post('create')
-  createUser(@Body() data: CreateUserDto) {
+  createUser(@Body() data: CreateUserDto): Promise<User> {
     return this.service.createUser(data);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,35 @@
 import { PrismaService } from './../prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private database: PrismaService) {}
 
-  async createUser(data: CreateUserDto) {
+  async createUser(createData: CreateUserDto): Promise<User> {
     const userExists = await this.database.user.findUnique({
-      where: { email: data.email },
+      where: { email: createData.email },
     });
 
     if (userExists) {
-      
+      throw new ConflictException('This e-mail is already in use.');
     }
+
+    const salt = 10;
+    const passHash = await bcrypt.hash(createData.pass, salt);
+
+    delete createData.passConfirm;
+
+    const user = await this.database.user.create({
+      data: {
+        ...createData,
+        pass: passHash,
+      },
+    });
+
+    delete user.pass;
+    return user;
   }
 }
